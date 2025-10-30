bc96837e54cfd3e8fc2df978ff7d05244e346bac
igwgw <hanseul0204@sookmyung.ac.kr>
2025-10-30 16:12:56 +0900

    04

--- main.c
+++ main.c
@@ -1,23 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define SIZE 5 //傈贸府扁 
+#define SIZE 4 //傈贸府扁 
+
+void square_array(int a[],int size);
+void print_array(int a[], int size); 
 
 int main(int argc, char *argv[])
 {
-   int i;
-   int a[SIZE]={1,2,3,4,5};
-   int b[SIZE]={1,8,3,4,5};
-   int flag_same=1; 
+   int list[SIZE]={1,2,3,4};
    
-   for(i=0;i<SIZE;i++)
-   {
-                      if (a[i]!=b[i])
-                     { printf("element is different. a[%d]=%d, b[%d]=%d\n",i,a[i],i,b[i]); 
-                      flag_same=0;}
-                      
-   }                   
-  
-  printf("Are a and b the same?%i\n",flag_same);
+   print_array(list, SIZE);
+   square_array(list, SIZE);
+   print_array(list, SIZE);
+   
   system("PAUSE");	
   return 0;
+}
+
+void square_array(int a[], int size)
+{
+     int i;
+     for(i=0;i<size;i++)
+     a[i]=a[i]*a[i];
+}
+
+void print_array(int a[], int size)
+{
+     int i;
+     for(i=0;i<size;i++)
+          printf("%3d",a[i]);
+     
+     printf("\n");
 }
