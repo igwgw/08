6d8ce6781ca2fdf2f1e9e9d4f3184ddbaebadc7f
igwgw <hanseul0204@sookmyung.ac.kr>
2025-10-30 16:04:56 +0900

    03_2

--- main.c
+++ main.c
@@ -5,19 +5,19 @@
 int main(int argc, char *argv[])
 {
    int i;
-   int grade[SIZE];
-   int score[SIZE];
+   int a[SIZE]={1,2,3,4,5};
+   int b[SIZE]={1,8,3,4,5};
+   int flag_same=1; 
    
    for(i=0;i<SIZE;i++)
-   grade[i]=rand()%100;
-   
-   for(i=0;i<SIZE;i++)
-   score[i]=grade[i];
-   
-   
-   for(i=0;i<SIZE;i++)
-   printf("score[%d]=%d and grade[%d]=%d\n",i,score[i],i,grade[i]); 
+   {
+                      if (a[i]!=b[i])
+                     { printf("element is different. a[%d]=%d, b[%d]=%d\n",i,a[i],i,b[i]); 
+                      flag_same=0;}
+                      
+   }                   
   
+  printf("Are a and b the same?%i\n",flag_same);
   system("PAUSE");	
   return 0;
 }
